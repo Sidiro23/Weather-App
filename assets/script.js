645b74e10ef060c6048e0a5005ae595a73e6c2ef
soutsos <106550353+Sidiro23@users.noreply.github.com>
2022-07-09 20:15:56 -0500

    JS/CSS/HTML changes

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Set global variables, including Open Weather Maps API Key
 var apiKey = "015dfb7f23707146caf5c855b7d20c55";
 var curCity = "";
-var preCity = "";
+var lCity = "";
 
 // Error  for fetch
 var handleErrors = (response) => {
@@ -24,29 +24,30 @@ var getCurConditions = (event)=>{
         return response.json();
     })
     .then((response)=>{
-        //save cit to local storage
-        saveCity(city);
+        //save city to local storage
+        sCity(city);
         $('error').text('');
         //current weather icon
         let curWeather="https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";  
         //get current time
+        let curTime = response.dt;
         let curTimeZone = response.timezone;
         let curTimeZoneHours = curTimeZone / 60 / 60;
-        let curMoment = moment.unix(curTime).utc().utcOffset(curTimeZOneHours);
+        let curMoment = moment.unix(curTime).utc().utcOffset(curTimeZoneHours);
         //render list of cities
         renderCities();
         //get 5 day forecast
         getFiveDay(event);
         //change header text to selected city
-        $('header-t').text(response.name);
+        $('#header-t').text(response.name);
         //create HTML for the search results
-        letcurWeatherHTML = `
-        <h3>${response.name} ${curmoment.format('(MM/DD/YY')}<img src='${curWeather}'></h3>
+        let curWeatherHTML = `
+        <h3>${response.name} ${curMoment.format('(MM/DD/YY)')}<img src='${curWeather}'></h3>
         <ul class='list'>
             <li>Temperature: ${response.main.temp}&#8457;</li>
             <li>Humidity: ${response.main.humidity}%</li>
             <li>Wind Speed: ${response.wind.speed}mph</li>
-            <li> id='uv'>UV Index:</li>
+            <li id='uv'>UV Index:</li>
             </ul>`;
             //apend results in DOM
             $('#current').html(curWeatherHTML);
@@ -54,6 +55,8 @@ var getCurConditions = (event)=>{
             let lat = response.coord.lat;
             let lon = response.coord.lon;
             let uvQURL = "api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&APPID=" + apiKey;
+            //added heroku url to resolve cors issue , so i can get the UV
+            uvQURL = ' https://salty-mountain-68764.herokuapp.com/' + uvQURL;
             //fetch for UV
             fetch(uvQURL)
             .then(handleErrors)
@@ -84,15 +87,15 @@ var getCurConditions = (event)=>{
                 fetch(qURL)
                 .then(handleErrors)
                 .then((response) =>{
-                    returnresponse.json()
+                    return response.json()
                 })
                 .then ((response) =>{
                     //create HTMl element with 5 day forecast
                     let fiveDayHTML =`
                     <h2>5 Day Forecast:</h2>
-                    <div id= 'five day' class = 'd-inline-flex flex-wrap'>`;
+                    <div id= 'five-day' class = 'd-inline-flex flex-wrap'>`;
                     //loop for 5 day forecast and display to html
-                    for ( let i = 0; i < responselist.length; i++){
+                    for ( let i = 0; i < response.list.length; i++){
                         let day = response.list[i];
                         let dayTime = day.dt;
                         let timeZone = response.city.timezone;
@@ -108,7 +111,7 @@ var getCurConditions = (event)=>{
                             <li class= 'weather'><img src='${picURL}'></li>
                             <li>Temp: ${day.main.temp}&#8457;</li>
                             <br>
-                            <li>Humidity: ${day.mai.humidity}%</li>
+                            <li>Humidity: ${day.main.humidity}%</li>
                             </ul>
                             </div>`;
                         }
@@ -124,7 +127,7 @@ var getCurConditions = (event)=>{
                 let cityOld = false;
                 //if already in storage
                 for (let i = 0; i< localStorage.length; i++) {
-                    if (localstorage['cities' + i] === nCity) {
+                    if (localStorage['cities' + i] === nCity) {
                         cityOld = true;
                         break;
                     }
@@ -134,4 +137,54 @@ var getCurConditions = (event)=>{
                     localStorage.setItem('cities' + localStorage.length, nCity);
                 }
             }
-            
+            //list of search cities
+            var renderCities = () => {
+                $('#results').empty();
+                //local storage empty
+                if (localStorage.length===0){
+                    if (lCity){
+                        $('#city').attr('value', lCity);
+
+                    }else {
+                        $('#city').attr('value','Austin');
+                    }
+                }else {
+                    let lCityKey ='cities'+(localStorage.length-1);
+                    lCity = localStorage.getItem(lCityKey);
+                    $('#city').attr('value',lCity);
+                    //saved cities to page
+                    for (let i = 0; i < localStorage.length; i++){
+                        let city = localStorage.getItem('cities' + i);
+                        let cityEl;
+                        if (curCity===''){
+                            curCity=lCity;
+                        }
+                        if(city===curCity){
+                            cityEl = `<button type='button' class='list-group-item list-group-item-action active'>${city}</button></li>`;
+                           } else {
+                            cityEl = `<button type='button' class='list-group-item list-group-item-action'>${city}</button></li>`;  
+                        }
+                        //append to page
+                        $('#results').prepend(cityEl);
+                    }
+                        if(localStorage.length>0){
+                            $('#storage').html($('<a id="storage" href="#">clear</a>'));
+                        } else{
+                            $('#storage').html('');
+                        }
+                    
+                }
+            }
+            //eventlistener
+            $('#button').on('click', (event)=> {
+                event.preventDefault();
+                curCity = $('#city').val();
+                getCurConditions(event);
+            });
+            //previous search eventlistener
+            $('#storage').on('click', (event)=>{
+                localStorage.clear();
+                renderCities();
+            });
+            renderCities();
+            getCurConditions();
